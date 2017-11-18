807ebd8dfd072c2dd736d096b6935a10fb08e24f
Esther Bustamante <estherluciabus@gmail.com>
2017-11-18 12:06:09 -0500

    agregue variables

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -6,7 +6,27 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'about.html'
 })
 export class AboutPage {
-  
+
+nombre:string = "Esther Bustamante";
+ocupacion:string ="Estudiante";
+edad:number = 15;
+
+datos: any = {
+  nombre:"Esther Bustamante",
+  ocupacion:"Estudiante",
+  edad:15
+};
+
+origen1:any ={
+   titulo_origen:"Origen",
+   origen:"Lima,Peru",
+   titulo_Email:"E-mail",
+   email:"estherluciabus@gmail.com",
+   titulo_telefono:"Telefono",
+   telefono:937825848
+
+};
+
   constructor(public navCtrl: NavController) {
 
   }
